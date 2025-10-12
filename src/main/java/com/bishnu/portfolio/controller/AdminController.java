b8ad72619d18cca96471cf1e44c3162c31a085d8
Bishnu <beurabishnuprasad14@gmail.com>
2025-10-12 14:45:19 +0530

    Done

--- src/main/java/com/bishnu/portfolio/controller/AdminController.java
+++ src/main/java/com/bishnu/portfolio/controller/AdminController.java
@@ -1,5 +1,13 @@
 package com.bishnu.portfolio.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class AdminController {
-    
+    @GetMapping("/admin-dashboard")
+    public String showAdminDashboard() {
+        return "admin-dashboard"; 
+    }  
 }
